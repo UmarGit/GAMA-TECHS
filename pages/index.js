2c66c5f6c7ecc800b852002a754728706f015f23
UmarGit <umarsheikh303@gmail.com>
2021-04-12 12:28:07 +0500

    Gama Techs

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,49 @@
 import Head from 'next/head'
+import { useState, useEffect } from "react";
 import styles from '../styles/Home.module.css'
 
+const words = ["Gama Techs", "Coming Soon..", "Get Ready ! for", "Gama Techs, In 2021"];
+
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const [subIndex, setSubIndex] = useState(0);
+  const [blink, setBlink] = useState(true);
+  const [reverse, setReverse] = useState(false);
+
+  // typeWriter
+  useEffect(() => {
+    if (index === words.length) return;
+
+    if (subIndex === words[index].length + 1 &&
+      index !== words.length - 1 && !reverse) {
+      setReverse(true);
+      return;
+    }
+
+    if (subIndex === 0 && reverse) {
+      setReverse(false);
+      setIndex((prev) => prev + 1);
+      return;
+    }
+
+    const timeout = setInterval(() => {
+      setSubIndex((prev) => prev + (reverse ? -1 : 1));
+    }, 
+    
+    Math.max(reverse ? 75 : subIndex === words[index].length ? 1500 : 150, parseInt(Math.random() * 350)));
+
+    return () => clearInterval(timeout);
+  }, [subIndex, index, reverse]);
+
+
+  // blinker
+  useEffect(() => {
+    const timeout2 = setTimeout(() => {
+      setBlink((prev) => !prev);
+    }, 1000);
+    return () => clearTimeout(timeout2);
+  }, [blink]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,57 +51,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="mx-auto w-auto">
+        <div className="text-6xl font-extrabold text-white p-4 text-center animate-pulse">
+          {`${words[index].substring(0, subIndex)}${blink ? "|" : " "}`}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
